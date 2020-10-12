e101dd951ed63817b099b57ba74f104b93a22257
korbutds <sys@hohou.ru>
2020-10-12 16:42:15 +0300

    Обработал текст ошибок получения данных от сервера

--- js/backend.js
+++ js/backend.js
@@ -1,21 +1,34 @@
 'use strict';
 
 (() => {
-  const URL = `https://21.javascript.pages.academy/code-and-magick/data`;
 
-  const loadData = function (onLoad) {
+  const loadData = function (onLoad, onError) {
+    const URL = `https://21.javascript.pages.academy/code-and-magick/data`;
+    const STATUS_CODE = {
+      OK: 200
+    };
+    const TIMEOUT_IN_MS = 10000;
+
     const xhr = new XMLHttpRequest();
     xhr.responseType = `json`;
 
+    xhr.addEventListener(`load`, function () {
+      if (xhr.status === STATUS_CODE.OK) {
+        onLoad(xhr.response);
+      } else {
+        onError(`Статус ответа: ${xhr.status} ${xhr.statusText}`);
+      }
+    });
+    xhr.addEventListener(`error`, function () {
+      onError(`Произошла ошибка соеденения`);
+    });
+    xhr.addEventListener(`timeout`, function () {
+      onError(`Запрос не успел выполниться за ${xhr.timeout}мс`);
+    });
+    xhr.timeout = TIMEOUT_IN_MS;
     xhr.open(`GET`, URL);
 
-    xhr.addEventListener(`load`, function () {
-      onLoad(xhr.response);
-    });
-
     xhr.send();
-
-
   };
 
   const saveData = () => {};
